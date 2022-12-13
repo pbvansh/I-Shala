97fa234865c9518f348eeeb4449c7aa19ec027d3
vidhi000 <vidhi5410rana@gmail.com>
2022-12-13 10:58:59 +0530

    resume

--- components/AddProjectModal.js
+++ components/AddProjectModal.js
@@ -12,7 +12,7 @@ const AddProjectModal = ({ setProjectModal, projects, setProjects }) => {
                 className="fixed inset-0 w-full h-full bg-black opacity-40"
                 onClick={() => setProjectModal(false)}
             ></div>
-            <div className="flex min-h-screen items-center justify-center">
+            <div className="shadow-md flex min-h-screen items-center justify-center">
                 <div className=" p-4 w-full max-w-xl h-full md:h-auto">
                     <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
                         <button onClick={() => setProjectModal(false)} type="button" className="absolute  top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
@@ -23,14 +23,14 @@ const AddProjectModal = ({ setProjectModal, projects, setProjects }) => {
                             <img src='https://t3.ftcdn.net/jpg/02/96/27/28/240_F_296272864_7XXe0wavzRXA9zKSkzXxhmZhQbyY8b26.jpg' height={100} width={100} className='mx-auto p-1' />
                             <div className='mb-5 whitespace-nowrap'>
                                 <div>
-                                    <p className="text-2xl font-bold text-sky-500">Academic/ personal project</p>
+                                    <p className="text-2xl font-semibold text-sky-500">Academic/ personal project</p>
                                     <div className="flex p-3 flex-col">
                                         <label className="font-semibold p-[5px] text-gray-700">Title</label>
                                         <input defaultValue={project.title} onChange={(e) => {
                                             setProject({
                                                 ...project, title: e.target.value
                                             })
-                                        }} type={'text'} placeholder='e.g. MyProjectTitle' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                                        }} type={'text'} placeholder='e.g. My Project Title' className="shadow-md  flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                                     </div>
                                     <div className="flex p-3 space-x-5">
                                         <div className="flex  flex-col flex-1 text-gray-700">
@@ -39,7 +39,7 @@ const AddProjectModal = ({ setProjectModal, projects, setProjects }) => {
                                                 setProject({
                                                     ...project, sMonth: e.target.value
                                                 })
-                                            }} type={'text'} placeholder='yyyy-mm' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                                            }} type={'text'} placeholder='yyyy-mm' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                                         </div>
                                         <div className="flex flex-col flex-1">
                                             <label className="font-semibold p-[5px] text-gray-700">End month</label>
@@ -47,7 +47,7 @@ const AddProjectModal = ({ setProjectModal, projects, setProjects }) => {
                                                 setProject({
                                                     ...project, eMonth: e.target.value
                                                 })
-                                            }} type={'text'} placeholder='yyyy-mm' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                                            }} type={'text'} placeholder='yyyy-mm' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                                         </div>
                                     </div>
                                     <div className="flex p-3 flex-col">
@@ -56,18 +56,18 @@ const AddProjectModal = ({ setProjectModal, projects, setProjects }) => {
                                             setProject({
                                                 ...project, desc: e.target.value
                                             })
-                                        }} type={'text'} placeholder='Short description about project (max 250 char)' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                                        }} type={'text'} placeholder='Short description about project (max 250 char)' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                                     </div>
                                     <div className="flex p-3 flex-col">
-                                        <label className="font-semibold p-[5px] text-gray-700">Project link <span className="text-xs">(Optional)</span></label>
+                                        <label className="font-semibold p-[5px] text-gray-700">Project link <span className="shadow-md text-xs">(Optional)</span></label>
                                         <input defaultValue={project.link} onChange={(e) => {
                                             setProject({
                                                 ...project, link: e.target.value
                                             })
-                                        }} type={'text'} placeholder='e.g. http://myprojectlink.com ' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                                        }} type={'text'} placeholder='e.g. http://myprojectlink.com ' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                                     </div>
                                     <div className="flex justify-end p-3">
-                                        <button onClick={AddProject} className="p-2 px-8 bg-sky-500 text-lg text-white rounded-sm hover:bg-sky-600">Save</button>
+                                        <button onClick={AddProject} className="p-2 boredr rounded-md px-8 bg-sky-500 text-lg text-white font-semibold hover:bg-sky-600">Save</button>
                                     </div>
                                 </div>
                             </div>
--- pages/student/resume.js
+++ pages/student/resume.js
@@ -78,63 +78,63 @@ const Resume = () => {
 
     return (
         <section className="min-h-screen max-w-4xl mx-auto border p-10 m-5 rounded-md space-y-8">
-            <p className="text-center font-bold text-4xl p-5">Your Resume</p>
+            <p className="text-center font-semibold text-4xl p-5">Your Resume</p>
             <div>
                 <div className="flex p-3 space-x-10">
-                    <input defaultValue={resume?.Fname} onChange={(e) => setFName(e.target.value)} type={'text'} placeholder='First Name' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
-                    <input defaultValue={resume?.Lname} onChange={(e) => setLName(e.target.value)} type={'text'} placeholder='Last Name' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    <input defaultValue={resume?.Fname} onChange={(e) => setFName(e.target.value)} type={'text'} placeholder='First Name' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    <input defaultValue={resume?.Lname} onChange={(e) => setLName(e.target.value)} type={'text'} placeholder='Last Name' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                 </div>
                 <div className="flex p-3 space-x-10">
-                    <input defaultValue={resume?.email} onChange={(e) => setEmail(e.target.value)} type={'text'} placeholder='Email - xyz@gmail.com' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
-                    <input defaultValue={resume?.contact} onChange={(e) => setContact(e.target.value)} type={'text'} placeholder='Mobile No.' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    <input defaultValue={resume?.email} onChange={(e) => setEmail(e.target.value)} type={'text'} placeholder='Email - xyz@gmail.com' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    <input defaultValue={resume?.contact} onChange={(e) => setContact(e.target.value)} type={'text'} placeholder='Mobile No.' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                 </div>
                 <div className="flex p-3">
-                    <textarea defaultValue={resume?.address} onChange={(e) => setAddress(e.target.value)} placeholder="Enter your address" className="flex-1 focus:outline-none border focus:border-sky-500 p-2" />
+                    <textarea defaultValue={resume?.address} onChange={(e) => setAddress(e.target.value)} placeholder="Enter your address" className="shadow-md flex-1 focus:outline-none border focus:border-sky-500 p-2" />
                 </div>
             </div>
             <hr />
             <div>
-                <p className="text-2xl font-bold text-sky-500">Graduation details</p>
+                <p className="text-2xl font-semibold text-sky-500">Graduation details</p>
                 <div className="flex p-3 flex-col">
                     <label className="font-bold p-[5px] text-gray-700">College</label>
                     <input defaultValue={resume?.education?.college} onChange={(e) => {
                         setEducation({ ...education, college: e.target.value })
-                    }} type={'text'} placeholder='e.g. Hindu College' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    }} type={'text'} placeholder='e.g. Hindu College' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                 </div>
                 <div className="flex p-3 flex-col">
                     <label className="font-bold p-[5px] text-gray-700">Degree</label>
                     <input defaultValue={resume?.education?.degree} onChange={(e) => {
                         setEducation({ ...education, degree: e.target.value })
-                    }} type={'text'} placeholder='e.g. B.Sc' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    }} type={'text'} placeholder='e.g. B.Sc' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                 </div>
                 <div className="flex p-3 space-x-5">
                     <div className="flex  flex-col flex-1 text-gray-700">
                         <label className="font-bold p-[5px]">Start year</label>
                         <input defaultValue={resume?.education?.sDate} onChange={(e) => {
                             setEducation({ ...education, sDate: e.target.value })
-                        }} type={'text'} placeholder='Choose Your' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                        }} type={'text'} placeholder='Choose Your' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                     </div>
                     <div className="flex flex-col flex-1">
                         <label className="font-bold p-[5px] text-gray-700">End year</label>
                         <input defaultValue={resume?.education?.eDate} onChange={(e) => {
                             setEducation({ ...education, eDate: e.target.value })
-                        }} type={'text'} placeholder='Choose Your' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                        }} type={'text'} placeholder='Choose Your' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                     </div>
                     <div className="flex flex-col flex-1">
                         <label className="font-bold p-[5px] text-gray-700">Performance</label>
                         <input defaultValue={resume?.education?.gPoint} onChange={(e) => {
                             setEducation({ ...education, gPoint: e.target.value })
-                        }} type={'text'} placeholder='0.00 (GP/10)' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                        }} type={'text'} placeholder='0.00 (GP/10)' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                     </div>
                 </div>
             </div>
             <hr />
             <div>
-                <p className="text-2xl font-bold text-sky-500">Academic/ personal project</p>
+                <p className="text-2xl font-semibold text-sky-500">Academic/ personal project</p>
                 <div>
                     {
                         projects.map((p, i) => (
-                            <div key={i} className="border p-3 m-5 rounded-md">
+                            <div key={i} className="border p-3 m-5 rounded-md shadow-md">
                                 <p className="font-semibold">{p.title}</p>
                                 <p>{p.sMonth} - {p.eMonth}</p>
                                 <a href={p.like} className='text-sky-600 cursor-pointer'>{p.link}</a>
@@ -145,8 +145,8 @@ const Resume = () => {
                     }
                 </div>
                 <div className="p-5">
-                    <p onClick={() => setProjectModal(true)} className="flex items-center hover:text-sky-500 cursor-pointer">
-                        <PlusIcon className="h-5 px-1" /> Add academic/ personal project
+                    <p onClick={() => setProjectModal(true)} className="flex items-center font-semibold hover:text-sky-500 cursor-pointer">
+                        <PlusIcon className="h-5 px-1 font-semibold" /> Add academic/ personal project
                     </p>
                 </div>
                 {/* <div className="flex p-3 flex-col">
@@ -174,7 +174,7 @@ const Resume = () => {
             </div>
             <hr />
             <div>
-                <p className="text-2xl font-bold text-sky-500">Skills</p>
+                <p className="text-2xl font-semibold text-sky-500">Skills</p>
                 <div className="flex mt-2 p-3">
                     {
                         skillsArray.map((s, i) => s.trim() !== '' && (
@@ -184,12 +184,12 @@ const Resume = () => {
                 </div>
                 <div className="flex p-3 flex-col">
                     <label className="font-bold p-[5px] text-gray-700">Add skills</label>
-                    <textarea defaultValue={resume?.skills} onChange={(e) => setSkills(e.target.value)} type={'text'} placeholder='e.g. C++,Java,PHP,etc...' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    <textarea defaultValue={resume?.skills} onChange={(e) => setSkills(e.target.value)} type={'text'} placeholder='e.g. C++,Java,PHP,etc...' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                 </div>
             </div>
             <hr />
             <div>
-                <p className="text-2xl font-bold text-sky-500">Certificate</p>
+                <p className="text-2xl font-semibold text-sky-500">Certificate</p>
                 <div className="flex mt-2 p-3">
                     {
                         certificateArray.map((c, i) => c.trim() !== '' && (
@@ -198,13 +198,13 @@ const Resume = () => {
                     }
                 </div>
                 <div className="flex p-3 flex-col">
-                    <label className="font-bold p-[5px] text-gray-700">Add Certificate</label>
-                    <textarea defaultValue={resume?.certificate} onChange={(e) => setCertificate(e.target.value)} multiline={true} type={'text'} placeholder={'Put certificate link here like this \nABC,\nXYZ,\nETC...'} className="flex-1 min-h-[120px] whitespace-pre-line rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    <label className="font-semibold p-[5px] text-gray-700">Add Certificate</label>
+                    <textarea defaultValue={resume?.certificate} onChange={(e) => setCertificate(e.target.value)} multiline={true} type={'text'} placeholder={'Put certificate link here like this \nABC,\nXYZ,\nETC...'} className="shadow-md flex-1 min-h-[120px] whitespace-pre-line rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                 </div>
             </div>
             <hr />
             <div>
-                <p className="text-2xl font-bold text-sky-500">portfolio /work samples</p>
+                <p className="text-2xl font-semibold text-sky-500">portfolio /work samples</p>
                 <div className="flex mt-2 p-3">
                     {
                         works.map((w, i) => w.trim() !== '' && (
@@ -213,19 +213,19 @@ const Resume = () => {
                     }
                 </div>
                 <div className="flex p-3 flex-col">
-                    <label className="font-bold p-[5px] text-gray-700">Add work samples</label>
-                    <textarea defaultValue={resume?.Portfolio_or_works} onChange={(e) => setPortfolio_or_works(e.target.value)} type={'text'} placeholder='e.g. C++,Java,PHP,etc...' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    <label className="font-semibold p-[5px] text-gray-700">Add work samples</label>
+                    <textarea defaultValue={resume?.Portfolio_or_works} onChange={(e) => setPortfolio_or_works(e.target.value)} type={'text'} placeholder='e.g. C++,Java,PHP,etc...' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                 </div>
             </div>
             <hr />
             <div>
-                <p className="text-2xl font-bold text-sky-500">accomplishments/ additional details</p>
+                <p className="text-2xl font-semibold text-sky-500">accomplishments/ additional details</p>
                 <div>
-                    youe skills here
+                    {/* Your skills are here */}
                 </div>
                 <div className="flex p-3 flex-col">
-                    <label className="font-bold p-[5px] text-gray-700">Additional details</label>
-                    <textarea defaultValue={resume?.Accomplishments} onChange={(e) => setAccomplishments(e.target.value)} type={'text'} placeholder='Write anything' className="flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
+                    <label className="font-semibold p-[5px] text-gray-700">Additional details</label>
+                    <textarea defaultValue={resume?.Accomplishments} onChange={(e) => setAccomplishments(e.target.value)} type={'text'} placeholder='Write anything' className="shadow-md flex-1 rounded-sm p-2 focus:outline-none border focus:border-sky-500" />
                 </div>
             </div>
             {
@@ -233,7 +233,7 @@ const Resume = () => {
                     : null
             }
             <div className="flex justify-end">
-                <button onClick={SaveResume} className="p-2 px-10 bg-sky-500 hover:bg-sky-600 rounded-sm text-white">Save Resume</button>
+                <button onClick={SaveResume} className="p-2 border rounded-md px-10 bg-sky-500 font-semibold hover:bg-sky-600  text-white shadow-md">Save Resume</button>
             </div>
         </section>
     )
